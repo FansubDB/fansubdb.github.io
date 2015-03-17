cb15ba524967cc83e0d9a3c60f372e0cac9dee4c
Fansub <fansub@mail-me.com>
2015-03-17 21:20:15 +0100

    externalize js

---
+++ js/image.js
@@ -0,0 +1,7 @@
+function image(link) {
+	document.getElementById('picture').src = link;
+}
+
+function reset () {
+	image('data:image/png;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
+}
