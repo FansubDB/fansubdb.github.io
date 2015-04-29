550172293346a3b75823d00e7eed37478955185f
Fansub <fansub@mail-me.com>
2015-04-29 17:03:55 +0200

    test row

--- js/data.js
+++ js/data.js
@@ -7,7 +7,9 @@ function jsonReader(link) {
   		if (req.readyState == 4) { //4 == XMLHttpRequest.DONE ie8+
      			if((req.status == 200) || (req.status == 304)) {
 				var objJson = JSON.parse(req.responseText); 
-				tableBuilder(objJson);
+				//tableBuilder(objJson);
+				rowBuilder(objJson);
+				
 			}
      			else {
 				messageTable('Fail to load table…');
@@ -50,6 +52,45 @@ function tableBuilder(arr){
 		dataTable += '</td>';
 	}
 	dataTable += '</tbody>';
+	
+	//write dataTable
+	var b_tableAnime = document.getElementById('tableAnime');
+        var Newb_tableAnime = b_tableAnime.cloneNode(false);
+        Newb_tableAnime.innerHTML = dataTable;
+        b_tableAnime.parentNode.replaceChild(Newb_tableAnime, b_tableAnime);
+}
+
+function rowBuilder(arr){
+	removeTable();
+	var dataTable = '';
+	for(i = 0; i < arr.anime.length; ++i) {
+		dataTable += '<div class="col-lg-4">';
+		dataTable += '<img class="img-circle" src="' + arr.anime[i].image + '" width="140" height="140">';
+		dataTable += '<h3>' + arr.anime[i].name + '</h3>';
+		dataTable += '<p>';
+		for (j = 0; j < arr.anime[i].group.length; ++j) {
+			dataTable += '<span class="' + arr.anime[i].group[j].status +'">';
+			for (k = 0; k < arr.anime[i].group[j].detail.length; ++k) {
+				if(arr.anime[i].group[j].detail[k].url) {
+					dataTable += '<a href="' + arr.anime[i].group[j].detail[k].url + '" target="_blank" >' + arr.anime[i].group[j].detail[k].name + '</a>';
+				}
+				else {
+					dataTable += arr.anime[i].group[j].detail[k].name;
+				}
+				if(k != arr.anime[i].group[j].detail.length-1) {
+					dataTable += ' ' + String.fromCharCode(38) + ' ';
+				}
+			}
+			dataTable += '</span>';
+			if(j != arr.anime[i].group.length-1) {
+				dataTable += '<br>';
+			}
+		}
+		if (arr.anime[i].group.length === 0) {
+			dataTable += 'N/A';
+		}
+		dataTable += '</p>';
+	}
 	
 	//write dataTable
 	var b_tableAnime = document.getElementById('tableAnime');
