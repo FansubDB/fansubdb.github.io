123e5b8755e37a332f91e9a5bcbde289f9a1bc56
FansubDB <fansubdb@mail-me.com>
2017-09-29 14:26:37 +0200

    Create settings.js

---
+++ js/settings.js
@@ -0,0 +1,12 @@
+/* Some global var */
+var DEBUG = true;
+var URL_DATA = "https://fansubdb.github.io/data/";
+
+var HOME = 1;
+var ARCHIVE = 2;
+var SEASON = 3;
+
+/*In "onclick" of the button, TV = 0 (default), OVA = 1 and MOVIE = 2.*/
+var TV = 0;
+var OVA = 1;
+var MOVIE = 2;
