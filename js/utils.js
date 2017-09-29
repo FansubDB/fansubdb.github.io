5f77737804103548ba89a2b6fc501870ac28182c
FansubDB <fansubdb@mail-me.com>
2017-09-29 14:27:08 +0200

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,64 @@
+/* Some defaults functions */
+function capitalizeFirstLetter(string) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function noSpace(string) {
+	return string.replace(/\s+/g, ''); /*https://stackoverflow.com/a/5963202*/
+}
+
+function copyToClipboard(text) {
+	window.prompt("Copy to clipboard: Ctrl+C, Enter", decodeText(text));
+}
+
+function encodeText(text) {
+	return encodeURIComponent(text);
+}
+
+function decodeText(text) {
+	return decodeURIComponent(text).replace(/&apos;/g, "'");
+}
+
+function writeMessage(id, string) {
+	document.getElementById(id).innerHTML = string;
+}
+
+function removeTag(id) {
+	document.getElementById(id).innerHTML = '';
+}
+
+function updateButton(id){
+	document.getElementById(id).className = "btn btn-default";
+}
+
+function isActive(id){
+	document.getElementById(id).className += " active";
+}
+
+function writeDataInnerHtml(id, data) {
+	var selectedElement = document.getElementById(id);
+	var newSelectedElement = selectedElement.cloneNode(false);
+	newSelectedElement.innerHTML = data;
+	selectedElement.parentNode.replaceChild(newSelectedElement, selectedElement);
+}
+
+function cantLoadImage(source, title) {
+	source.src = "";
+	source.onerror = "";
+	writeLog(">Fail to load picture of: " + decodeText(title));
+	return true;
+}
+
+function writeLog(text) {
+	if (DEBUG) {
+		console.log(text);
+	}
+}
+
+function warningTemplate(text) {
+	return '<div class="alert alert-warning"><a href="#" class="close" data-dismiss="alert">&times;</a>' + text + '</div>';
+}
+
+function infoTemplate(text) {
+	return '<div class="alert alert-info"><a href="#" class="close" data-dismiss="alert">&times;</a>' + text + '</div>';
+}
