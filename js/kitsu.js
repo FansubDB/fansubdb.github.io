816c58dbd23719517108a6745936532952c47c50
FansubDB <fansubdb@mail-me.com>
2017-10-02 11:13:35 +0200

    Update kitsu.js

--- js/kitsu.js
+++ js/kitsu.js
@@ -1,30 +1,33 @@
-function infoKitsu(anime) {
-	var url = "https://kitsu.io/api/edge/";
+function infoKitsu(anime, kitsu_id) {
+	if(kitsu_id) {
+		var url = "https://kitsu.io/api/edge/";
 
-	var req = new XMLHttpRequest();
-	console.log('Loading data…');
-	req.open('GET', url + "anime/?page[limit]=1&filter[text]=" + anime, true); //true for asynchronous
+		var req = new XMLHttpRequest();
+		console.log('Loading data…');
+		req.open('GET', url + "anime/" + kitsu_id, true); //true for asynchronous
 
-	req.setRequestHeader("Accept", "application/vnd.api+json");
-	req.setRequestHeader("Content-Type", "application/vnd.api+json");
-	
-	req.onreadystatechange = function () {
-		if (req.readyState == 4) { //4 == XMLHttpRequest.DONE ie8+
-			if((req.status == 200) || (req.status == 304)) {
-				var objJson = JSON.parse(req.responseText);
-				addInfo(anime, objJson.data[0]);
+		req.setRequestHeader("Accept", "application/vnd.api+json");
+		req.setRequestHeader("Content-Type", "application/vnd.api+json");
+		
+		req.onreadystatechange = function () {
+			if (req.readyState == 4) { //4 == XMLHttpRequest.DONE ie8+
+				if((req.status == 200) || (req.status == 304)) {
+					var objJson = JSON.parse(req.responseText);
+					addInfo(anime, kitsu_id, objJson.data);
+				}
 			}
-		}
-	};
-	req.send(null);
+		};
+		req.send(null);
+	}
 }
 
-function addInfo(anime, arr) {
+function addInfo(anime, kitsu_id, arr) {
 	removeTag('info_' + noSpace(anime));
 	console.log('Adding kitsu info…');
 	var dataInfo = '';
 
-	dataInfo += '<li>Number of Episode: ' + arr.attributes.episodeCount + '</li>';
+	dataInfo += '<li><img src="' + arr.attributes.posterImage.small + '" width="200px"></li>';
+	dataInfo += '<li>Number of Episode: ' + episodeNumber(arr.attributes.episodeCount) + '</li>';
 	dataInfo += '<li><a href="https://kitsu.io/anime/' + arr.id + '" target="_blank" title="' + arr.attributes.titles.en_jp + '">Kitsu URL</a></li>';
 
 	writeDataInnerHtml('info_' + noSpace(anime), dataInfo);
