aabebafa69b62f7d6c9b172457d13b8e9eb59737
FansubDB <fansubdb@mail-me.com>
2018-06-10 19:33:25 +0200

    remove useless code

--- javascript/utils.js
+++ javascript/utils.js
@@ -43,14 +43,13 @@ function writeDataInnerHtml(id, data) {
 }
 
 function cantLoadImage(source, title) {
-	if (typeof source.src === 'undefined' || source.src === "") {
+	if (source === "") {
 		writeLog("> No picture to display for: " + decodeText(title));
 	}
 	else {
 		writeLog("> Fail to load picture of: " + decodeText(title));
 	}
-	source.src = "";
-	source.onerror = "";
+
 	return true;
 }
 
