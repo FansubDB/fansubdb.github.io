24764cbde4371f73a3f5d11c77d3d48e9fb48fc1
FansubDB <fansubdb@mail-me.com>
2018-06-10 18:58:32 +0200

    Update utils.js

--- javascript/utils.js
+++ javascript/utils.js
@@ -43,14 +43,14 @@ function writeDataInnerHtml(id, data) {
 }
 
 function cantLoadImage(source, title) {
-	source.src = "";
-	source.onerror = "";
 	if (typeof source.src === 'undefined' || source.src === "") {
 		writeLog("> No picture to display for: " + decodeText(title));
 	}
 	else {
 		writeLog("> Fail to load picture of: " + decodeText(title));
 	}
+	source.src = "";
+	source.onerror = "";
 	return true;
 }
 
